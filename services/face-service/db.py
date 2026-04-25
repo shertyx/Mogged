6458e8164cad52472be3a7a48eaa6b2913649216
cdecottignies <decottignies.clement@gmail.com>
2026-04-25 23:30:59 +0200

    fix(face-service): fix invalid row_to_json SQL in get_cached_score

--- services/face-service/db.py
+++ services/face-service/db.py
@@ -14,13 +14,24 @@ class DBClient:
         with self._conn() as conn:
             with conn.cursor() as cur:
                 cur.execute(
-                    "SELECT chad_score, row_to_json(face.scores) FROM face.scores WHERE photo_hash = %s",
+                    """SELECT chad_score, symmetry, golden_ratio, jawline, eyes, nose, forehead
+                       FROM face.scores WHERE photo_hash = %s""",
                     (photo_hash,),
                 )
                 row = cur.fetchone()
                 if row is None:
                     return None
-                return {"chad_score": row[0], "features": row[1]}
+                return {
+                    "chad_score": row[0],
+                    "features": {
+                        "symmetry": row[1],
+                        "golden_ratio": row[2],
+                        "jawline": row[3],
+                        "eyes": row[4],
+                        "nose": row[5],
+                        "forehead": row[6],
+                    },
+                }
 
     def save_score(
         self,
